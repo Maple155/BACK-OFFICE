155899964f7361997f9b099e888927a66ab3a27a
Maple155 <rantoluciano622@gmail.com>
2026-03-12 14:11:43 +0300

    Update .sql

--- sql/.sql
+++ sql/.sql
@@ -11,7 +11,7 @@ TRUNCATE TABLE Vehicules_Reservations, Mission, Distances, Reservation, Vehicule
 
 -- Paramètres attendus par AssignmentService
 INSERT INTO Parametres (id, libelle, value, code) VALUES
-(1, 'Vitesse moyen (km / h)', '40', 'VITESSE_MOYENNE_KMH'),
+(1, 'Vitesse moyen (km / h)', '50', 'VITESSE_MOYENNE_KMH'),
 (2, 'Temps d''attente (min)', '0', 'TEMPS_ATTENTE_MIN');
 
 -- Lieux (IMPORTANT : id=1 = aéroport, utilisé en dur dans AssignmentService)
@@ -21,7 +21,8 @@ INSERT INTO Lieu (id, code, libelle) VALUES
 (3, 'ANL', 'Analakely'),
 (4, 'AMH', 'Ambohimanarina'),
 (5, 'AMB', 'Ambatobe'),
-(6, 'ITA', 'Itaosy');
+(6, 'ITA', 'Itaosy'),
+(7, 'HTL', 'hotel 1');
 
 -- Distances minimales nécessaires pour les calculs de circuit
 INSERT INTO Distances (id_from, id_to, kilometer) VALUES
@@ -37,7 +38,8 @@ INSERT INTO Distances (id_from, id_to, kilometer) VALUES
 (3, 5, 9.1),
 (4, 5, 6.9),
 (5, 6, 11.2),
-(4, 6, 13.0);
+(4, 6, 13.0),
+(1, 7, 50);
 
 -- Types de carburant
 INSERT INTO TypeCarburant (id, libelle) VALUES
@@ -46,23 +48,25 @@ INSERT INTO TypeCarburant (id, libelle) VALUES
 
 -- Flotte de test (inclut Diesel pour la règle de priorité)
 INSERT INTO Vehicule (id, reference, nbPlaces, typeCarburant_id) VALUES
-(1, 'VH-TEST-001', 4, 1),
-(2, 'VH-TEST-002', 6, 2),
-(3, 'VH-TEST-003', 8, 2);
+(1, 'VH-TEST-001', 12, 2),
+(2, 'VH-TEST-002', 5, 1),
+(3, 'VH-TEST-003', 5, 2),
+(4, 'VH-TEST-004', 12, 1);
 
 -- Cas principal demandé : même date/heure EXACTE, lieux différents
 -- Toutes ces réservations sont à 2026-03-10 09:00:00 mais destinations différentes
 INSERT INTO Reservation (id, id_lieu, client, nbPassager, dateHeure) VALUES
-(1, 2, 'CLI-1001', 2, '2026-03-10 09:00:00'),
-(2, 3, 'CLI-1002', 1, '2026-03-10 09:00:00'),
-(3, 4, 'CLI-1003', 2, '2026-03-10 09:00:00'),
-(4, 5, 'CLI-1004', 1, '2026-03-10 09:00:00'),
-(5, 6, 'CLI-1005', 7, '2026-03-10 09:00:00');
+(1, 7, 'CLI-1001', 7, '2026-03-12 09:00:00'),
+(2, 7, 'CLI-1002', 11, '2026-03-12 09:00:00'),
+(3, 7, 'CLI-1003', 3, '2026-03-12 09:00:00'),
+(4, 7, 'CLI-1004', 1, '2026-03-12 09:00:00'),
+(5, 7, 'CLI-1005', 2, '2026-03-12 09:00:00'),
+(6, 7, 'CLI-1006', 20, '2026-03-12 09:00:00');
 
 -- Données complémentaires optionnelles (autre créneau)
-INSERT INTO Reservation (id, id_lieu, client, nbPassager, dateHeure) VALUES
-(6, 3, 'CLI-2001', 2, '2026-03-10 10:30:00'),
-(7, 5, 'CLI-2002', 1, '2026-03-10 10:30:00');
+-- INSERT INTO Reservation (id, id_lieu, client, nbPassager, dateHeure) VALUES
+-- (6, 3, 'CLI-2001', 2, '2026-03-10 10:30:00'),
+-- (7, 5, 'CLI-2002', 1, '2026-03-10 10:30:00');
 
 -- Remise à niveau des séquences
 SELECT setval('parametres_id_seq', COALESCE((SELECT MAX(id) FROM Parametres), 1));
